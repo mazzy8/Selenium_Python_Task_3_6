26f0c278ba0e8a02c3b00c2238e763aad98ee5c0
mazzy_test <test@mail.com>
2022-09-18 14:30:43 +0500

    worked version of test

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
