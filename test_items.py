723b74bb8c8c5530b6fe176f3a91e4b9ee889314
mazzy_test <test@mail.com>
2022-09-18 16:23:27 +0500

    add working assert finish version

--- test_items.py
+++ test_items.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_find_card_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    text_button = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert text_button, 'Card button not find'
+
