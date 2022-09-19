25bbc8fc905a31d85ce32a3940d43fc1fe98d54f
mazzy <mazzy8@mail.ru>
2022-09-19 13:25:09 +0500

    add modul time and time.sleep

--- test_items.py
+++ test_items.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
-
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_find_card_button(browser):
     browser.get(link)
+    time.sleep(10)
     text_button = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
     assert text_button, 'Card button not find'
 
