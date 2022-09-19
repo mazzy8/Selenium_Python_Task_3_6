b87e383507cce2b1c00c50076fa0fceff5697dac
Maxim Butuzov <101450815+mazzy8@users.noreply.github.com>
2022-09-19 12:48:43 +0500

    Update conftest.py
    del time.sleep(30)

--- conftest.py
+++ conftest.py
@@ -28,5 +28,4 @@ def browser(request):
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
-    time.sleep(30)
     browser.quit()
